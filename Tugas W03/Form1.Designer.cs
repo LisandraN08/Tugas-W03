934973fa2d6f68aa7436cd875b4e72c3df500299
LisandraN08 <lnicoline@student.ciputra.ac.id>
2022-03-13 19:28:46 +0700

    Add project files.

---
+++ Tugas W03/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿
+namespace Tugas_W03
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtBoxInput = new System.Windows.Forms.TextBox();
+            this.lblInput = new System.Windows.Forms.Label();
+            this.btnProses = new System.Windows.Forms.Button();
+            this.lblOutput = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtBoxInput
+            // 
+            this.txtBoxInput.Location = new System.Drawing.Point(34, 62);
+            this.txtBoxInput.Name = "txtBoxInput";
+            this.txtBoxInput.Size = new System.Drawing.Size(200, 22);
+            this.txtBoxInput.TabIndex = 0;
+            // 
+            // lblInput
+            // 
+            this.lblInput.AutoSize = true;
+            this.lblInput.Location = new System.Drawing.Point(31, 42);
+            this.lblInput.Name = "lblInput";
+            this.lblInput.Size = new System.Drawing.Size(77, 17);
+            this.lblInput.TabIndex = 1;
+            this.lblInput.Text = "Input Data:";
+            // 
+            // btnProses
+            // 
+            this.btnProses.Location = new System.Drawing.Point(34, 107);
+            this.btnProses.Name = "btnProses";
+            this.btnProses.Size = new System.Drawing.Size(75, 23);
+            this.btnProses.TabIndex = 2;
+            this.btnProses.Text = "Proses";
+            this.btnProses.UseVisualStyleBackColor = true;
+            this.btnProses.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.AutoSize = true;
+            this.lblOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblOutput.Location = new System.Drawing.Point(31, 144);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(85, 24);
+            this.lblOutput.TabIndex = 3;
+            this.lblOutput.Text = "[EMPTY]";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(453, 291);
+            this.Controls.Add(this.lblOutput);
+            this.Controls.Add(this.btnProses);
+            this.Controls.Add(this.lblInput);
+            this.Controls.Add(this.txtBoxInput);
+            this.Name = "Form1";
+            this.Text = "Form Pengaturan Warna";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtBoxInput;
+        private System.Windows.Forms.Label lblInput;
+        private System.Windows.Forms.Button btnProses;
+        private System.Windows.Forms.Label lblOutput;
+    }
+}
+
---
+++ Tugas W03/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tugas_W03
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            sistemKerja();
+        }
+        public void sistemKerja()
+        {
+            double ukuranFontAwal = 10.8;
+            string coba1 = "";
+            coba1 = txtBoxInput.Text;
+            string[] teksAkhir = coba1.Split(':');
+            if (coba1.Contains(':'))
+            {
+                teksAkhir[0] = teksAkhir[0].ToUpper();
+                teksAkhir[1] = teksAkhir[1].ToUpper();
+            }
+            else
+            {
+                coba1 = coba1.ToUpper();
+            }
+            if (teksAkhir[0] == "ISI")
+            {
+                string text = "";
+                text = txtBoxInput.Text;
+                string[] textAkhir = text.Split(':');
+                lblOutput.Text = textAkhir[1];
+            }
+            else if (coba1 == "SHOWN")
+            {
+                lblOutput.Show();
+            }
+            else if (coba1 == "HIDE")
+            {
+                lblOutput.Hide();
+            }
+            else if (teksAkhir[0] == "WARNA")
+            {
+                if (teksAkhir[1].Contains("DEFAULT"))
+                {
+                    lblOutput.ForeColor = Color.Black;
+                }
+                else if (teksAkhir[1].Contains("MERAH"))
+                {
+                    lblOutput.ForeColor = Color.Red;
+                }
+                else if (teksAkhir[1].Contains("HIJAU"))
+                {
+                    lblOutput.ForeColor = Color.Green;
+                }
+                else if (teksAkhir[1].Contains("BIRU"))
+                {
+                    lblOutput.ForeColor = Color.Blue;
+                }
+            }
+            else if (coba1 == "RESTART")
+            {
+                lblOutput.Text = "[EMPTY]";
+                lblOutput.ForeColor = Color.Black;
+                lblOutput.Font = new Font("Microsoft Sans Serif", 10.8F);
+            }
+            else if (coba1 == "BESARKAN")
+            {
+                lblOutput.Font = ChangeFontSize(lblOutput.Font, lblOutput.Font.Size+2);
+            }
+            else if (coba1 == "KECILKAN")
+            {
+                lblOutput.Font = ChangeFontSize(lblOutput.Font, lblOutput.Font.Size-2);
+            }
+            else
+            {
+                MessageBox.Show("Syntax tidak terdaftar!");
+            }
+        }
+        static public Font ChangeFontSize(Font font, float fontSize)
+        {
+            if (font != null)
+            {
+                float currentSize = font.Size;
+                if (currentSize != fontSize)
+                {
+                    font = new Font(font.Name, fontSize, font.Style, font.Unit, font.GdiCharSet, font.GdiVerticalFont);
+                }
+            }
+            return font;
+        }
+
+    }
+}
